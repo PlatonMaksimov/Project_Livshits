8b200bbcfc85e00d6a3266c3e3c2b7706d7db803
PlatonMaksimov <plathon.livshits@yandex.ru>
2025-10-08 17:48:42 +0300

    Вариант 13 сделан

--- PZ_2/PZ_2_1.py
+++ PZ_2/PZ_2_1.py
@@ -1,8 +1,32 @@
-num = 12
+#Вариант №13
+#Дано двузначное число. Вывести число, полученное при перестановки цифр исходного числа.
 
-a = num // 10
-b = num % 10
+def reverse_number():
+    while True:
+        try:
+            num = input('Введите двузначное число: ')
 
-rev_num = b * 10 + a
+            if not num.isdigit():
+                raise ValueError('Ошибка: Введены нечисловые данные!')
 
-print(rev_num)
+            if len(num) != 2:
+                raise ValueError('Ошибка: Число должно быть двузначным!')
+
+            num_int = int(num)
+
+            reverse_num = num[1] + num[0]
+
+            print(f'Исходное число {num}')
+            print(f'Число после перестановки цифр: {reverse_num}')
+
+            break
+
+        except ValueError as e:
+            print(f"Ошибка ввода: {e}")
+            print("Пожалуйста, попробуйте еще раз.\n")
+
+        except Exception as e:
+            print(f"Произошла непредвиденная ошибка: {e}")
+            print("Пожалуйста, попробуйте еще раз.\n")
+
+reverse_number()
